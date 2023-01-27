75f78e2a6d47a5d6c6024e5a0271b16ed7f18d9b
Mohammad Hossein Movahedi <m.h.movahedi97@gmail.com>
2023-01-27 22:26:53 +0000

    my age in dog years

--- 1. Build a Passenger Counter App/3. Setting up our JavaScript file/Index.js
+++ 1. Build a Passenger Counter App/3. Setting up our JavaScript file/Index.js
@@ -1,4 +1,16 @@
 // document.getElementById("count-el").innerText = 5
 let count = 0
 
-console.log(count) 
+// console.log(count) 
+// // create a variable called myAge and set it equal to your age
+// let myAge = 25
+// // call the console.log() function and pass in the myAge variable
+// console.log(myAge)
+
+// create two variables called MyAge and humanDogRatio
+let myAge = 25
+let humanDogRatio = 7
+// create a variable called myDogAge and set it equal to myAge multiplied by humanDogRatio
+let myDogAge = myAge * humanDogRatio
+// call the console.log() function and pass in the myDogAge variable
+console.log(myDogAge)
