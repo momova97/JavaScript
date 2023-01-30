cfee2d2ef379eedea3667271ac2aa1a5de553230
Mohammad Hossein Movahedi <m.h.movahedi97@gmail.com>
2023-01-30 16:47:37 +0000

    changes

--- 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
+++ 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
@@ -1,6 +1,11 @@
 let lap1 = 34
 let lap2 = 33
-let lap3 = 36
+let lap3 = 35
 
 // Create a function that logs out the sum of all the lap times
 
+function sumlaps() {
+    let sum = lap1 + lap2 + lap3
+    console.log(sum)
+}
+sumlaps()
--- 1. Build a Passenger Counter App/12. Write a function that increments/index.js
+++ 1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -3,7 +3,10 @@ let lapsCompleted = 0
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
 
-
+function addLap() {
+    lapsCompleted = lapsCompleted + 1
+}
 
 
 console.log(lapsCompleted)
+addLap()
