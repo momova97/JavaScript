ca0780ac1797411b9187a76edeab8aab3cd01940
Mohammad Hossein Movahedi <m.h.movahedi97@gmail.com>
2023-02-08 22:26:44 +0000

    incerment test 1

--- 1. Build a Passenger Counter App/13. Increment on clicks/index.js
+++ 1. Build a Passenger Counter App/13. Increment on clicks/index.js
@@ -5,8 +5,14 @@
 // console.log(count)
 
 // intialize the count as 0
+let count = 0
 // listen for clicks on the increment button
+function increment() {
+    count = count + 1
+    console.log("The button was clicked")
+}
 // increment the count variable when the button is clicked
+
 // change the count-el in the HTML to reflect the new count
 
 
